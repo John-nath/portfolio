42f123fa5e4aa143d71ddd4998d4fe7295758dbd
john-nath <jadikwu22@gmail.com>
2026-03-20 12:59:12 +0100

    Date corrected

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -16,7 +16,7 @@ const Footer = ({isDarkMode}) => {
       </div>
 
       <div className='text-center sm:flex items-center justify-between border-t border-gray-400 mx-[10%] mt-12 py-6'>
-        <p>Copyright © 2025. John Nath. All rights reserved </p>
+        <p>Copyright © {new Date().getFullYear()}. John Nath. All rights reserved </p>
         <ul className='flex items-center gap-10 justify-center mt-4 sm:mt-0 '>
             <li><a target='_blank' href="https://github.com/John-nath">Github</a></li>
             <li><a target='_blank' href="https://www.linkedin.com/in/john-adikwu-322413200/">LinkedIn</a></li>
